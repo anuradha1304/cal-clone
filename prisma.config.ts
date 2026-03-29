732e0dc82265904e7ee8b5e671713689b3493a02
anuradha1304 <anuradha1304@users.noreply.github.com>
2026-03-29 17:10:30 +0530

    remove prisma config

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-import { defineConfig } from 'prisma/config'
-
-export default defineConfig({
-  schema: path.join('prisma', 'schema.prisma'),
-  seed: {
-    run: 'node prisma/seed.mjs',
-  },
-})
